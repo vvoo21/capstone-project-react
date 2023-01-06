8a62eba90a27325862d6a886b0c7ae7c2df7e0b9
Vanessa O <vaneolipa@gmail.com>
2023-01-05 21:44:00 -0500

    Fix linters error

--- src/__tests__/Navigation.test.js
+++ src/__tests__/Navigation.test.js
@@ -1,6 +1,6 @@
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navigation from '../components/navigation';
+import Navigation from '../components/Navigation';
 
 it('renders correctly', () => {
   const navigation = renderer
